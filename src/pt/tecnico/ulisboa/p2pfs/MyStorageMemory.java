8a8f9a105c9c173709a562ad1c0c83272fac4b6d
afatc <alexandrefilipecampos@gmail.com>
2013-12-09 21:07:50 -0800

    Mais cenas no MyPeerMaker e MyStorageMemory, so falta ter em contas as partes do ficheiro

--- src/pt/tecnico/ulisboa/p2pfs/MyStorageMemory.java
+++ src/pt/tecnico/ulisboa/p2pfs/MyStorageMemory.java
@@ -1,8 +1,10 @@
 package pt.tecnico.ulisboa.p2pfs;
 
 import java.io.IOException;
+import java.util.NavigableMap;
 
 import net.tomp2p.peers.Number160;
+import net.tomp2p.peers.Number480;
 import net.tomp2p.storage.Data;
 import net.tomp2p.storage.StorageMemory;
 
@@ -10,6 +12,12 @@ import net.tomp2p.storage.StorageMemory;
 public class MyStorageMemory extends StorageMemory{
 	
 	private final StatAggregator stat = new StatAggregator();
+	private Number160 myOwnerPeerId;
+	
+	public MyStorageMemory(Number160 myOwnerPeerId){
+		super();
+		this.myOwnerPeerId = myOwnerPeerId;
+	}
 	
 	@Override
 	public boolean put(Number160 locationKey, Number160 domainKey, Number160 contentKey, Data value){
@@ -18,16 +26,16 @@ public class MyStorageMemory extends StorageMemory{
 		try {
 			if(value.getObject() instanceof MetaStub){
 				
-				stat.addMB(value.getData().length * 1000 * 1000); //Byte to MB... and not Byte to Megabibyte
+				stat.addMBMeta(value.getData().length * 1000 * 1000); //Byte to MB... and not Byte to Mebibyte
 				
 			}else{
 			if(value.getObject() instanceof FileStub){
 			
-				stat.addMB(value.getData().length * 1000 * 1000);
-				stat.addFile(1); //TODO
+				stat.addMBFiles(value.getData().length * 1000 * 1000);
+				stat.addFile(1); //TODO ver as partes
 				
 			}else{
-				System.out.println("@MyStorageMemory: ERRO FICHEIRO RECEBIDO NAO E META NEM FILE");
+				System.out.println("@MyStorageMemory put: ERRO FICHEIRO RECEBIDO NAO E META NEM FILE");
 			}}
 			
 		} catch (ClassNotFoundException e) {
@@ -45,57 +53,115 @@ public class MyStorageMemory extends StorageMemory{
 	public Data remove(Number160 locationKey, Number160 domainKey, Number160 contentKey){
 		Data ret = super.remove(locationKey, domainKey, contentKey);
 		
+		try {
+			if(ret.getObject() instanceof MetaStub){
+				
+				stat.remMBMeta(ret.getData().length * 1000 * 1000); //Byte to MB... and not Byte to Mebibyte
+				
+			}else{
+			if(ret.getObject() instanceof FileStub){
+			
+				stat.remMBFiles(ret.getData().length * 1000 * 1000);
+				stat.remFile(1); //TODO ver as partes
+				
+			}else{
+				System.out.println("@MyStorageMemory remove: ERRO FICHEIRO RECEBIDO NAO E META NEM FILE");
+			}}
+			
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return ret;
 	}
 
 	public int getNumberOfRootMetaFilesImResponsibleFor(){
 		
-		return 0;
+		NavigableMap<Number480, Data> map = super.map();
+		int rootMetasIOwn = 0;
+		
+		for(Number480 n : map.descendingKeySet()){
+			Data d = map.get(n);
+			try {
+				if(d.getObject() instanceof MetaStub){
+					
+					if(((MetaStub) d.getObject()).isRoot()){
+						
+						if(this.myOwnerPeerId.compareTo(super.findPeerIDForResponsibleContent(n.getLocationKey())) == 0){
+							rootMetasIOwn++;
+						}
+					}
+				}
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return rootMetasIOwn;
 	}
 	
 	private class FileStub{}
-	private class MetaStub{}
+	private class MetaStub{
+
+		public boolean isRoot() {
+			// TODO Auto-generated method stub
+			return false;
+		}
+	}
 	
-	
+	public double getNumStoredFiles(){
+		return stat.getNumStoredFiles();
+	}
+	public double getNumMBFiles(){
+		return stat.getNumMBFiles();
+	}
+	public double getNumMBMeta(){
+		return stat.getNumMBMeta();
+	}
 	private class StatAggregator{
 		
 		private double nFiles;
-		private double nMB;
-		
+		private double nMBFiles;
+		private double nMBMeta;
+
+		public void addMBMeta(double mb) {
+			this.nMBMeta += mb;
+		}
+		public void remMBMeta(double mb){
+			nMBMeta -= mb;
+		}
 		public StatAggregator(){
 			nFiles = 0;
-			nMB = 0;
+			nMBFiles = 0;
+			nMBMeta = 0;
 		}
-		
 		public void addFile(double part){
-			
 			nFiles += part;
 		}
-		
 		public void remFile(double part){
-			
 			nFiles -= part;
 		}
-		
-		public void addMB(double mb){
-			
-			nMB += mb;
+		public void addMBFiles(double mb){
+			nMBFiles += mb;
 		}
-		
-		public void remMB(double mb){
-			
-			nMB -= mb;
+		public void remMBFiles(double mb){	
+			nMBFiles -= mb;
 		}
-		
 		public double getNumStoredFiles(){
-			
 			return nFiles;
 		}
-		
-		public double getNumMegaBytes(){
-			
-			return nMB;
+		public double getNumMBFiles(){	
+			return nMBFiles;
+		}
+		public double getNumMBMeta() {
+			return nMBMeta;
 		}
 	}
 }
