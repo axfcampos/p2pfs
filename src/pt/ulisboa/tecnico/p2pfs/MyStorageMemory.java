4fc0e2b7be84227b45524fb415d8538c585d288c
afatc <alexandrefilipecampos@gmail.com>
2013-12-11 16:31:43 -0800

    Made the statAggr class public

--- src/pt/ulisboa/tecnico/p2pfs/MyStorageMemory.java
+++ src/pt/ulisboa/tecnico/p2pfs/MyStorageMemory.java
@@ -155,49 +155,5 @@ public class MyStorageMemory extends StorageMemory{
 //	public void printStats(){
 ////		System.out.println(stat.toString());
 //	}
-	private class StatAggregator{
-		
-		private double nFiles;
-		private double nMBFiles;
-		private double nMBMeta;
 
-		public void setMBMeta(double mb) {
-			this.nMBMeta = mb;
-		}
-//		public void remMBMeta(double mb){
-//			nMBMeta -= mb;
-//		}
-		public StatAggregator(){
-			nFiles = 0;
-			nMBFiles = 0;
-			nMBMeta = 0;
-		}
-		public void setNumFile(double part){
-			nFiles = part;
-		}
-//		public void remFile(double part){
-//			nFiles -= part;
-//		}
-		public void setMBFiles(double mb){
-			nMBFiles = mb;
-		}
-//		public void remMBFiles(double mb){	
-//			nMBFiles -= mb;
-//		}
-		@SuppressWarnings("unused")
-		public double getNumStoredFiles(){
-			return nFiles;
-		}
-		@SuppressWarnings("unused")
-		public double getNumMBFiles(){	
-			return nMBFiles;
-		}
-		@SuppressWarnings("unused")
-		public double getNumMBMeta() {
-			return nMBMeta;
-		}
-		public String toString(){
-			return "nFiles: " + nFiles + "\n nMBFIles: " + nMBFiles + "\n nMBMeta: " + nMBMeta + "\n"; 
-		}
-	}
 }
